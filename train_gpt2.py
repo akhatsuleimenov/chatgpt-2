60f4075dcd18f9b843320ac07edafc12a3d18060
Akhat <akhatsuleimenov@gmail.com>
2024-08-05 16:28:46 -0700

    autocast to bfloat16

--- train_gpt2.py
+++ train_gpt2.py
@@ -222,6 +222,8 @@ class DataLoaderLite:
 
 # --------------------------------------------------------------------------------
 
+import time
+
 device = "cpu"
 if torch.cuda.is_available():
     device = "cuda"
@@ -233,7 +235,9 @@ torch.manual_seed(1337)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(1337)
 
-train_loader = DataLoaderLite(B=4, T=32)
+train_loader = DataLoaderLite(B=16, T=1024)
+
+torch.set_float32_matmul_precision("high")
 
 # get logits
 # model = GPT.from_pretrained("gpt2")
@@ -243,13 +247,21 @@ model.to(device)
 # optimizing
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    logits, loss = model(x, y)
+    with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}: loss: {loss.item()}")
+    # torch.cuda.synchronize()
+    t1 = time.time()
+    dt = (t1 - t0) * 1000
+    tokers_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+    print(
+        f"step {i}: loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokers_per_sec:.2f}"
+    )
 
 import sys
 
